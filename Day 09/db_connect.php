bd7cb9ae416de82c7e2f1ff91b13db804f2389c3
mohitsharma3013 <mohitkhandal100@gmail.com>
2026-07-08 12:25:53 +0530

    Implement student registration in db_connect.php
    Added functionality to register a student in the database using POST method.

--- Day 09/db_connect.php
+++ Day 09/db_connect.php
@@ -1,1 +1,36 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
+$name = mysqli_real_escape_string($conn, $_POST['name']);
+
+$email = mysqli_real_escape_string($conn, $_POST['email']);
+
+$branch = mysqli_real_escape_string($conn, $_POST['branch']);
+
+$cgpa = $_POST['cgpa'];
+
+$sql = "INSERT INTO students (name, email, branch, cgpa) VALUES ('$name', '$email', '$branch', '$cgpa')";
+
+if (mysqli_query($conn, $sql)) {
+
+echo "Student Registered Successfully!";
+
+} else {
+
+echo "Error: " . mysqli_error($conn);
+
+}
+}
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "skit";
+
+$conn = mysqli_connect($host, $user, $password, $database);
+
+if (!$conn) {
+    die("Connection Failed: " . mysqli_connect_error());
+}
+
+ echo "Connection Successful!";
+?>
