8fb8a578011802e318fd5511b28279f96c03b336
mohitsharma3013 <mohitkhandal100@gmail.com>
2026-07-08 23:28:40 +0530

    Add database connection script in db_connectnew.php

--- Day 10/db_connectnew.php
+++ Day 10/db_connectnew.php
@@ -1,1 +1,14 @@
+<?php
 
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "industrial training";
+
+$conn = mysqli_connect($host, $user, $password, $database);
+
+if (!$conn) {
+    die("Connection Failed: " . mysqli_connect_error());
+}
+
+?>
