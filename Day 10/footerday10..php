62cb4c0325149038aad35c7f88f2cd9f8cf81ec3
mohitsharma3013 <mohitkhandal100@gmail.com>
2026-07-08 23:34:19 +0530

    Add footer with copyright notice and Bootstrap JS

--- Day 10/footerday10..php
+++ Day 10/footerday10..php
@@ -1,1 +1,13 @@
+<footer class="bg-dark text-white text-center py-3 fixed bottom ">
 
+<p class="mb-0">&copy; <?php echo date("Y"); 
+?> My Website. All Rights Reserved.</p>
+
+</footer>
+
+<!-- Bootstrap JS --> <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.
+
+js">
+</script>
+</body>
+</html>
