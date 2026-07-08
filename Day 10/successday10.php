f2e1a561a7651577270546262db553ac1ad61619
mohitsharma3013 <mohitkhandal100@gmail.com>
2026-07-08 23:35:16 +0530

    Add success message for registration completion

--- Day 10/successday10.php
+++ Day 10/successday10.php
@@ -1,1 +1,18 @@
+<?php
 
+include ("headerday10.php");
+
+?> 
+<div class="container mt-5 text-center">
+
+<div class="alert alert-success">
+
+<h3>Registration Successful!</h3>
+<p>Your form has been submitted successfully.</p>
+</div>
+
+<a href="login.php" class="btn btn-primary">Go to Home</a>
+</div>
+ <?php
+ include ("footerday10.php");
+ ?>
