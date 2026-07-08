e056525f969de0418bde5875a224266861dbe3e0
mohitsharma3013 <mohitkhandal100@gmail.com>
2026-07-08 23:32:17 +0530

    Add header structure with navigation and login button

--- Day 10/headerday10.php
+++ Day 10/headerday10.php
@@ -1,1 +1,45 @@
+<html>
 
+<head>
+
+<meta charset="UTF-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>My Website</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<header class="bg-light border-bottom">
+
+<div class="container">
+
+<div class="d-flex justify-content-between
+    align-items-center py-3">
+     <!-- #region 
+      -->
+     <img src="banana.jpeg"
+     alt="banana" width="80">
+       <nav>
+        <ul class="nav">
+            
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+            </li>
+            </ul>
+            </nav>
+           <div class="col-3 text-end">
+            <button type="button" class="btn btn-primary">Login</button> </div>
+        </div>
+    </div>
+    </header>
