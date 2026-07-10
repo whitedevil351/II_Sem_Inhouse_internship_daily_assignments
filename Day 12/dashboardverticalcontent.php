5d350ce176ec5090a0c65905006c1ece91c5b146
mohitsharma3013 <mohitkhandal100@gmail.com>
2026-07-10 23:29:27 +0530

    Add links for updating password and profile

--- Day 12/dashboardverticalcontent.php
+++ Day 12/dashboardverticalcontent.php
@@ -1,1 +1,11 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-3">
 
+            <a href="updatepassword.php">Update Password</a>
+            <br>
+            <a href="updateprofile.php">Update Profile</a>
+
+        </div>
+
+        <div class="col-md-9">
