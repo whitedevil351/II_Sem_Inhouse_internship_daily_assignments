48f1b6c12c3f3027667a444ef48e7bf2a06b7590
mohitsharma3013 <mohitkhandal100@gmail.com>
2026-07-10 23:31:46 +0530

    Implement profile update form
    Added a form for updating user profile information, including name and file upload.

--- Day 12/updateprofile.php
+++ Day 12/updateprofile.php
@@ -1,1 +1,38 @@
+<?php
+  include("db_connect.php");
+  include("dashboardheader.php");
+  include("dashboardverticalcontent.php");
+  ?>
+
+
+ <div class="container mt-5" style="max-width:500px;">
+
+<h2 class="mb-3">Update Profile</h2>
+
+<form action="" method="POST">
+
+<input type="text"
+class="form-control mb-3"
+name="name"
+placeholder="Name" value="<?=$_SESSION['user_name']?>" required>
+
+<input type="file"
+class="form-control mb-3"
+name="file">
+
+
+<button type="submit" class="btn btn-primary w-100">
+Update
+</button>
+
+</form>
+
+</div>
+
+
+
+<?php
+  include("dashboardfooter.php");
+  include("footerday10.php");
+  ?>
 
